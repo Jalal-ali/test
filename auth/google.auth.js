419cf4da50486664fd4cb5defb37b9264de36a25
Jalal Ali <154836984+Jalal-ali@users.noreply.github.com>
2025-07-24 07:07:32 +0500

    Update google.auth.js
    this is id

--- auth/google.auth.js
+++ auth/google.auth.js
@@ -10,9 +10,9 @@ passport.deserializeUser(function (user, done) {
 });
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://my-cal-com-backend.vercel.app/google/callback",
+ clientID: "285629337150-1r0qe4cspfjd6n8d5onbtnouvejbkbcd.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "https://test-one-delta-84.vercel.app/google/callback",
   passReqToCallback: true
 }, (request, accessToken, refrestToken, profile, done) => {
   // console.log(profile);//! -------> Consoling profile in server
